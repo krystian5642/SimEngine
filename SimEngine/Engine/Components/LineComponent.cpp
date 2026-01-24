9ec5192431b1477a300612feb631911dda452e13
krystiansobczyk <krystian.sobczyk@alraunastudio.com>
2026-01-24 23:14:10 +0100

    add followParent option to draw parent trajectory (usefull in tracking celestial body trajectory)

--- SimEngine/Engine/Components/LineComponent.cpp
+++ SimEngine/Engine/Components/LineComponent.cpp
@@ -1,13 +1,25 @@
 ﻿#include "LineComponent.h"
 
 #include "Rendering/Renderer/Renderer.h"
+#include "Scene/Objects/Entities/Entity.h"
 
 LineComponent::LineComponent(ObjectBase* parent, Scene* scene, const std::string& name)
     : RenderComponent(parent, scene, name)
 {
     line = Renderer::CreateLineStatic();
 }
+
+void LineComponent::Tick(float deltaTime)
+{
+    RenderComponent::Tick(deltaTime);
     
+    if (followParent)
+    {
+        auto actor = dynamic_cast<Entity*>(parent);
+        AddPoint(actor->GetPosition());
+    }
+}
+
 void LineComponent::Draw(const std::shared_ptr<const Shader>& shader, bool visualPass) const
 {
     if (visualPass)
